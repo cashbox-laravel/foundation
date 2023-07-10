c3b9e39d8b60420ce300724000e2f101a754f1c0
Andrey Helldar <helldar@dragon-code.pro>
2023-07-11 02:18:20 +0300

    Update copyrights

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,3 +1,19 @@
+
+/**
+ * This file is part of the "cashier-provider/foundation" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ *
+ * @copyright 2023 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/cashier-provider/foundation
+ */
+
 import dotenv from 'dotenv'
 
 import { defaultTheme, viteBundler } from 'vuepress'
