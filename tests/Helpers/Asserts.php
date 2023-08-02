e68aba8f5ffe1251ed45a1e4038e948640acb3e4
Andrey Helldar <helldar@dragon-code.pro>
2023-08-02 22:28:00 +0300

    Expanded message output

--- tests/Helpers/Asserts.php
+++ tests/Helpers/Asserts.php
@@ -6,26 +6,33 @@ use Illuminate\Support\Str;
 use PHPUnit\Framework\Assert;
 use Tests\Fixtures\App\Enums\StatusEnum;
 
-expect()->extend('toBeHasCashbox', function (string $message = '') {
-    expect($this->value->cashbox()->exists())->toBeTrue($message);
+expect()->extend('toBeHasCashbox', function () {
+    expect($this->value->cashbox()->exists())->toBeTrue();
 
     return $this;
 });
 
-expect()->extend('toBeDoesntHaveCashbox', function (string $message = '') {
-    expect($this->value->cashbox()->doesntExist())->toBeTrue($message);
+expect()->extend('toBeDoesntHaveCashbox', function () {
+    expect($this->value->cashbox()->doesntExist())->toBeTrue();
 
     return $this;
 });
 
-expect()->extend('toBeUrl', function (string $message = '') {
+expect()->extend('toBeUrl', function () {
+    $message = sprintf('The value must be a valid URL: "%s"', $this->value);
+
     Assert::assertTrue(Str::isUrl($this->value), $message);
 
     return $this;
 });
 
 expect()->extend('toBeStatus', function (StatusEnum $status) {
-    expect($this->value->refresh()->status)->toBe($status);
+    $expected = sprintf('"%s" (%s)', $status->value, $status->name);
+    $actual   = sprintf('"%s" (%s)', $this->value->status->value, $this->value->status->name);
+
+    $message = sprintf('The status must be %s, %s given.', $expected, $actual);
+
+    expect($this->value->refresh()->status)->toBe($status, $message);
 
     return $this;
 });
